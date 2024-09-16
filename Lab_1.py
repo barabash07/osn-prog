9b078a5f35f212d671073f7ee212ffbc1f40262c
barabash07 <valeriabarabas905@gmail.com>
2024-09-16 12:06:19 +0300

    Add files via upload

---
+++ Lab_1.py
@@ -0,0 +1,11 @@
+text = ('Hello world')
+name = ('Valeriia')
+last_name = ('Barabash')
+age = 16
+print(text)
+print(name, last_name)
+print(age)
+print(type(text))
+print(type(name))
+print(type(last_name))
+print(type(age))
